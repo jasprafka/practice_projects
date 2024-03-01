0b20b9fc8ac10919133ea41d86508a476bc6ac96
Jake Sprafka <jasprafka@gmail.com>
2024-03-01 17:19:31 -0600

    Initial commit for graphics libs.

---
+++ c/glfw_projects/hello_glfw.c
@@ -0,0 +1,6 @@
+// include glfw here
+
+int main() {
+    // do something simple with glfw here
+    return 0;
+}
---
+++ c/raylib_projects/hello_raylib.c
@@ -0,0 +1,6 @@
+#include <raylib.h>
+
+int main() {
+    // do something simple with raylib here
+    return 0;
+}
